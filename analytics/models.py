7603a4d22772d3fd42359fa56e4ba6bf1768e105
Victor Zhu <zzy809@126.com>
2017-07-26 21:16:23 -0400

    Project Linted
    by flake8

--- analytics/models.py
+++ analytics/models.py
@@ -4,13 +4,14 @@ from django.db import models
 # Create your models here.
 from tags.models import Tag
 
+
 class TagAnalyticsManager(models.Manager):
 	def add_count(self, user, tag):
 		obj, created = self.model.objects.get_or_create(user=user, tag=tag)
 		obj.count += 1
 		obj.save()
 		return obj
-		
+
 
 class TagAnalytics(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
